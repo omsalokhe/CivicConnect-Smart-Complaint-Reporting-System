33cb455359cf1935f0a6301a8b129fad81ec8016
Om salokhe <omsalokhe2020@gmail.com>
2026-01-16 18:16:55 +0530

    Use environment variable for server port
    Updated server to use environment variable for port.

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,8 @@ mongoose.connect("mongodb://127.0.0.1:27017/complaintDB")
 .then(() => console.log("✅ MongoDB Connected"))
 .catch(err => console.log(err));
 
-app.listen(5000, () => {
-    console.log("🚀 Server running on http://localhost:5000");
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// app.listen(5000, () => {
+//     console.log("🚀 Server running on http://localhost:5000");
+// });
